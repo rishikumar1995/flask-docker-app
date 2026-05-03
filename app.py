a28b025eab5b35b73aa31456e684521193a2b527
Rishi Kadam <rk5570376@gmail.com>
2026-05-03 12:02:18 +0000

    added test route

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/test")
+def test():
+    return "Auto Deploy Working 🚀🔥"
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
